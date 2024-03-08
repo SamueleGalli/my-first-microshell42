a8a2233aa7fa9c31bf9d8337ea320b9752645c52
SamueleGalli <80041753+SamueleGalli@users.noreply.github.com>
2024-03-08 10:57:04 +0100

    Add files via upload

--- microshell.c
+++ microshell.c
@@ -1,144 +1,127 @@
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
 
-int len(char *s)
+int	len(char *s)
 {
-    int i = 0;
-    while (s[i])
-        i++;
-    return (i);
+	int	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-void    error(char *s, char *msg)
+void	error(char *s, char *msg)
 {
-    write(2, msg, len(msg));
-    if (s != NULL)
-        write(2, s, len(s));
-    write(2, "\n", 1);
+	write(2, msg, len(msg));
+	if (s)
+		write(2, s, len(s));
+	write(2, "\n", 1);
 }
 
-void    cding(char **v, int i)
+void	cding(char **v, int i)
 {
-    i++;
-    if (v[i] != NULL && v[i][0] == ';')
-        error(NULL, "error: cd: bad arguments");
-    else if (chdir(v[i]) == -1)
-        error(v[i], "cannot change directory to ");
+	i++;
+	if (v[i] && v[i][0] == ';')
+		error(NULL, "error: cd: bad arguments");
+	else if (chdir(v[i]) == -1)
+		error(v[i], "error: cd: cannot change directory to ");
 }
 
-int piping(char **v, int i)
+int	ispipe(char **v, int i)
 {
-    while (v[i] != NULL && v[i][0] != '|')
-        i++;
-    if (v[i] != NULL && v[i][0] == '|')
-        return (1);
-    return (0);
+	while(v[i] && v[i][0] != '|' &&v[i][0] != ';')
+		i++;
+	if (v[i] && v[i][0] == '|')
+		return (1);
+	return (0);
 }
 
-void    child(char **v, int fd[2], int i, char **env)
+void	piping(char **v, int i, char **e, pid_t p, int fd[2])
 {
-    int j = i;
-    if (close(fd[0]) == -1)
-        error(NULL, "error: fatal");
-    while (v[i] != NULL && v[i][0] != ';')
-    {
-        if (v[i][0] == '|')
-        {
-            if (dup2(fd[1], 1) == -1)
-                error(NULL, "error: fatal");
-            break;
-        }
-        i++;
-    }
-    v[i] = NULL;
-    if (close(fd[1]) == -1)
-        error(NULL, "error: fatal");
-    i = j;
-    if (execve(v[i], &v[i], env) == -1)
-        error(v[i], "error: cannot execute ");
-    exit(0);
+	while(v[i] && v[i][0] != ';')
+	{
+		pipe(fd);
+		p = fork();
+		if (p == 0)
+		{
+			int	j = i;
+			if (close(fd[0]) == -1)
+				error(NULL, "error: fatal");
+			while (v[i] && v[i][0] != ';')
+			{
+				if (v[i][0] == '|')
+				{
+					if (dup2(fd[1], 1) == -1)
+						error(NULL, "error: fatal");
+					break ;
+				}
+				i++;
+			}
+			v[i] = 0;
+			i = j;	
+			if (close(fd[1]) == -1)
+				error(NULL, "error: fatal");
+			if (execve(v[i], &v[i], e) == -1)
+				error(v[i], "error: cannot execute ");
+			exit(1);
+		}
+		else
+		{
+
+			if (close(fd[1]) == -1)
+				error(NULL, "error: fatal");
+			if (dup2(fd[0], 0) == -1)
+				error(NULL, "error: fatal");
+			if (close(fd[0]) == -1)
+				error(NULL, "error: fatal");
+			waitpid(p, 0, 0);
+		}
+		while (v[i] && v[i][0] != '|' && v[i][0] != ';')
+			i++;
+		if (v[i] && v[i][0] == '|')
+			i++;
+	}
 }
 
-void    pipe_exec(char **v, char **env, int i, pid_t pid, int fd[2])
+void	single(char **v, int i, char **e, pid_t p)
 {
-    while (v[i] != NULL && v[i][0] != ';')
-    {
-        if (pipe(fd) == -1)
-        {
-                error(NULL, "error: fatal");
-            exit(0);
-        }
-        pid = fork();
-        if (pid < 0)
-        {
-            error(NULL, "error: fatal");
-            exit(0);
-        }
-        if (pid == 0)
-            child(v, fd, i, env);
-        else
-        {
-            if (close(fd[1]) == -1)
-                error(NULL, "error: fatal");
-            if (dup2(fd[0], 0) == -1)
-                error(NULL, "error: fatal");
-            if (close(fd[0]) == -1)
-                error(NULL, "error: fatal");
-            waitpid(pid, NULL, 0);
-        }
-        while (v[i] != NULL && v[i][0] != ';' && v[i][0] != '|')
-            i++;
-        if (v[i] != NULL && v[i][0] == '|')
-            i++;
-    }
+	p = fork();
+	if (p == 0)
+	{
+		int j = i;
+		while (v[i] && v[i][0] != ';')
+			i++;
+		v[i] = 0;
+		i = j;
+		if (execve(v[i], &v[i], e) == -1)
+			error(v[i], "error: cannot execute ");
+	}
+	else
+		waitpid(p, 0, 0);
 }
 
-void    single(pid_t pid, char **env, int i, char **v)
+int	main(int c, char **v, char **e)
 {
-    pid = fork();
-    if (pid == -1)
-    {
-        error(NULL, "error: fatal");
-        exit(0);
-    }
-    if (pid == 0)
-    {
-        int j = i;
-        while (v[i] != NULL && v[i][0] != ';')
-            i++;
-        if (v[i] != NULL && v[i][0] == ';')
-            v[i] = NULL;
-        i = 0;
-        i = j;
-        if (execve(v[i], &v[i], env) == -1)
-            error (v[i], "error: cannot execute ");
-    }
-    else
-        waitpid(pid, 0, 0);
-}
+	(void)c;
+	int	i = 1;
+	int	fd[2];
+	pid_t	p = 0;
 
-int main(int c, char **v, char **env)
-{
-    (void)c;
-    int i = 1;
-    pid_t pid = 0;
-    int fd[2];
-    while (v[i] != NULL && v[i][0] == ';')
-        i++;
-    while (v[i])
-    {
-        if (strcmp(v[i], "cd") == 0)
-            cding(v, i);
-        else if (piping(v, i) == 1)
-            pipe_exec(v, env, i, pid, fd);
-        else
-        single(pid,env,i, v);
-        while (v[i] != NULL && v[i][0] != ';')
-            i++;
-        while (v[i] != NULL && v[i][0] == ';')
-            i++;
-    }
+	while (v[i] && v[i][0] == ';')
+		i++;
+	while (v[i])
+	{
+		if (strcmp(v[i], "cd") == 0)
+			cding(v, i);
+		else if (ispipe(v, i) == 1)
+			piping(v, i, e, p, fd);
+		else
+			single(v, i, e, p);
+		while (v[i] && v[i][0] != ';')
+			i++;
+		while (v[i] && v[i][0] == ';')
+			i++;
+	}
 }
